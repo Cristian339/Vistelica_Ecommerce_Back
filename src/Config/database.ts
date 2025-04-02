507cf79c9f392604c288458d583ddd267155e58c
jmoca <114067390+jmoca@users.noreply.github.com>
2025-04-02 22:03:40 +0200

    bd desplegada

--- src/Config/database.ts
+++ src/Config/database.ts
@@ -1,23 +1,37 @@
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+import { URL } from 'url';  // Asegúrate de importar 'URL' desde 'url'
+
 import { User } from '../Entities/User';
 import { Products } from '../Entities/Products';
-import {Category} from '../Entities/Category';
-import {Subcategory} from '../Entities/Subcategory';
-import {Review} from "../Entities/Review";
-import {Profile} from "../Entities/Profile";
-import {Order} from "../Entities/Order";
-import {OrderDetail} from "../Entities/OrderDetail";
-import {Wishlist} from "../Entities/Wishlist";
+import { Category } from '../Entities/Category';
+import { Subcategory } from '../Entities/Subcategory';
+import { Review } from "../Entities/Review";
+import { Profile } from "../Entities/Profile";
+import { Order } from "../Entities/Order";
+import { OrderDetail } from "../Entities/OrderDetail";
+import { Wishlist } from "../Entities/Wishlist";
+
+// Cargar variables de entorno
+dotenv.config();
+
+// Verificar si DATABASE_URL está definida
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+    throw new Error("DATABASE_URL no está definida en el archivo .env");
+}
+
+const url = new URL(databaseUrl); // Ahora puedes usar 'url' correctamente
 
 export const AppDataSource = new DataSource({
-    type: 'postgres', // Base de datos Postgres
-    host: 'localhost', // Dirección del host, puede ser localhost o la IP del servidor
-    port: 5432, // Puerto de PostgreSQL
-    username: 'postgres', // Usuario de la base de datos
-    password: 'root', // Contraseña de la base de datos
-    database: 'postgres', // Nombre de la base de datos
+    type: 'postgres',
+    host: url.hostname, // Extraer el host de la URL
+    port: parseInt(url.port), // Extraer el puerto de la URL
+    username: url.username, // Extraer el usuario de la URL
+    password: url.password, // Extraer la contraseña de la URL
+    database: url.pathname.split('/')[1],
     schema: 'vistelica',
-    entities: [User, Profile, Products, Category, Subcategory, Review,Order, OrderDetail, Wishlist], // Entidades que se van a mapear
+    entities: [User, Profile, Products, Category, Subcategory, Review, Order, OrderDetail, Wishlist],
     synchronize: true,
     logging: false,
     migrations: [],
