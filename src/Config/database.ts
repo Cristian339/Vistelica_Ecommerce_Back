b07efd70277f073b20c0d502f6fd09044f601d92
jmoca <114067390+jmoca@users.noreply.github.com>
2025-04-13 09:41:22 +0200

    actualizacion bd

--- src/Config/database.ts
+++ src/Config/database.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
-import { URL } from 'url';  // Asegúrate de importar 'URL' desde 'url'
+import { URL } from 'url';
 
 import { User } from '../Entities/User';
 import { Products } from '../Entities/Products';
@@ -11,33 +11,31 @@ import { Profile } from "../Entities/Profile";
 import { Order } from "../Entities/Order";
 import { OrderDetail } from "../Entities/OrderDetail";
 import { Wishlist } from "../Entities/Wishlist";
-import {Payment} from "../Entities/Payment";
-import {Supplier} from "../Entities/Supplier";
+import { Payment } from "../Entities/Payment";
+import { Supplier } from "../Entities/Supplier";
 
-// Cargar variables de entorno
 dotenv.config();
 
-// Verificar si DATABASE_URL está definida
 const databaseUrl = process.env.DATABASE_URL;
 if (!databaseUrl) {
     throw new Error("DATABASE_URL no está definida en el archivo .env");
 }
 
-const url = new URL(databaseUrl); // Ahora puedes usar 'url' correctamente
+const url = new URL(databaseUrl);
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: url.hostname, // Extraer el host de la URL
-    port: parseInt(url.port), // Extraer el puerto de la URL
-    username: url.username, // Extraer el usuario de la URL
-    password: url.password, // Extraer la contraseña de la URL
+    host: url.hostname,
+    port: parseInt(url.port || '5432'), // Usa 5432 si no viene en la URL
+    username: url.username,
+    password: url.password,
     database: url.pathname.split('/')[1],
     schema: 'vistelica',
-    entities: [User, Profile, Products, Category, Subcategory, Review, Order, OrderDetail, Wishlist, Payment,Supplier],
+    entities: [User, Profile, Products, Category, Subcategory, Review, Order, OrderDetail, Wishlist, Payment, Supplier],
     synchronize: true,
     logging: false,
     migrations: [],
-    subscribers: [],
+    subscribers: []
 });
 
 export default AppDataSource;
