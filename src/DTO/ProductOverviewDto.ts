f5421d1e12f74c9b7df6f52ed0c956e7dad4aedd
jmoca <114067390+jmoca@users.noreply.github.com>
2025-06-11 22:56:42 +0200

    constructor

--- src/DTO/ProductOverviewDto.ts
+++ src/DTO/ProductOverviewDto.ts
@@ -1,10 +1,10 @@
 export class ProductOverviewDto {
-    product_id: number;
-    name: string;
-    price: number;
-    colors: string[];
-    main_image: string;
-    average_rating: number;
-    subcategory_name: string;
-    reviews_count: number;
+    product_id!: number;
+    name!: string;
+    price!: number;
+    colors!: string[];
+    main_image!: string;
+    average_rating!: number;
+    subcategory_name!: string;
+    reviews_count!: number;
 }
